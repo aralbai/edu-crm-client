cbf873f8b6d0825f8f30bbcae0608ac9bb303c04
aralbai <xabibullayevmm@gmail.com>
2025-05-18 14:30:27 +0500

    changed

--- src/app/sing-in/page.tsx
+++ src/app/sing-in/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function LoginPage() {
-  return (
-    <div>
-      Loginpage
-    </div>
-  )
+  return <div>Loginpage</div>;
 }
